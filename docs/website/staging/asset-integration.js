78b7738610594d88b2adcb1689d49d1ce855ecf0
ladima75-source <ladima75@gmail.com>
2026-09-15 09:48:36 +0300

    TASK 24.5 force real PULS screenshots from embedded data

--- docs/website/staging/asset-integration.js
+++ docs/website/staging/asset-integration.js
@@ -1,1 +1,44 @@
-(()=>{const x=new XMLHttpRequest();x.open('GET','asset-integration-base.js',false);x.send(null);if(x.status>=200&&x.status<300)(0,eval)(x.responseText);const task=document.createElement('script');task.src='task24-3-product-modules.js?v=20260914-2444';task.async=false;document.head.appendChild(task);const how=document.createElement('script');how.src='task24-4-how-it-works.js?v=20260914-2450';how.async=false;document.head.appendChild(how);const modes=document.createElement('script');modes.src='task24-5-irrigation-modes.js?v=20260915-0625';modes.async=false;document.head.appendChild(modes)})();
+(()=>{
+  const x=new XMLHttpRequest();
+  x.open('GET','asset-integration-base.js',false);
+  x.send(null);
+  if(x.status>=200&&x.status<300)(0,eval)(x.responseText);
+
+  const task=document.createElement('script');
+  task.src='task24-3-product-modules.js?v=20260914-2444';
+  task.async=false;
+  document.head.appendChild(task);
+
+  const how=document.createElement('script');
+  how.src='task24-4-how-it-works.js?v=20260914-2450';
+  how.async=false;
+  document.head.appendChild(how);
+
+  const patchPuls=()=>{
+    const imgs=document.querySelectorAll('#actual.task245 .task245-shot img');
+    if(imgs[0]&&window.TASK245_VOL) imgs[0].src=window.TASK245_VOL;
+    if(imgs[1]&&window.TIME) imgs[1].src=window.TIME;
+  };
+
+  const vol=document.createElement('script');
+  vol.src='task24-5-volume-data.js?v=20260915-0648';
+  vol.async=false;
+  vol.onload=()=>{
+    const time=document.createElement('script');
+    time.src='task24-5-time-data.js?v=20260915-0648';
+    time.async=false;
+    time.onload=()=>{
+      const modes=document.createElement('script');
+      modes.src='task24-5-irrigation-modes.js?v=20260915-0648';
+      modes.async=false;
+      modes.onload=()=>{
+        requestAnimationFrame(()=>requestAnimationFrame(patchPuls));
+        setTimeout(patchPuls,120);
+        setTimeout(patchPuls,500);
+      };
+      document.head.appendChild(modes);
+    };
+    document.head.appendChild(time);
+  };
+  document.head.appendChild(vol);
+})();
