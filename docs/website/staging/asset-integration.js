49f0ebb4c95b2eab8fdc7b3d0d3fa78313e4d918
ladima75-source <ladima75@gmail.com>
2026-09-15 21:24:41 +0300

    TASK 24.6.1 refresh visual review assets

--- docs/website/staging/asset-integration.js
+++ docs/website/staging/asset-integration.js
@@ -19,20 +19,14 @@
   modes.async=false;
   modes.onload=()=>{
     const imgs=document.querySelectorAll('#actual.task245 .task245-shot img');
-    if(imgs[0]){
-      imgs[0].src='task24-5-volume.webp?v=20260915-1657';
-      imgs[0].removeAttribute('srcset');
-    }
-    if(imgs[1]){
-      imgs[1].src='task24-5-time.webp?v=20260915-1657';
-      imgs[1].removeAttribute('srcset');
-    }
+    if(imgs[0]){imgs[0].src='task24-5-volume.webp?v=20260915-1657';imgs[0].removeAttribute('srcset');}
+    if(imgs[1]){imgs[1].src='task24-5-time.webp?v=20260915-1657';imgs[1].removeAttribute('srcset');}
     const fert=document.createElement('script');
     fert.src='task24-6-fertigation.js?v=20260915-2462';
     fert.async=false;
     fert.onload=()=>{
       const polish=document.createElement('script');
-      polish.src='task24-6-1-polish.js?v=20260915-2462';
+      polish.src='task24-6-1-polish.js?v=20260915-2463';
       polish.async=false;
       document.head.appendChild(polish);
     };
